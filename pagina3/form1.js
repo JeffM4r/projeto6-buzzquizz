5e4a07269806b89b64de1e36b6e5a4ad6e15e90e
Igor Tamuz <igortamuz@gmail.com>
2022-07-04 16:28:55 -0300

    Js form1

--- pagina3/form1.js
+++ pagina3/form1.js
@@ -1,36 +1,64 @@
 let quizzSend = "https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes"
 
 let continueQuestionsButton = document.querySelector(".continueQuestionsButton")
-continueQuestionsButton.addEventListener("click", reciveForm1)
+continueQuestionsButton.addEventListener("click", saveValues)
 
+let tittleQuizzForm1
+let urlQuizzForm1 
+let quantQuestionQuizzForm1 
+let quantNivelQuizzForm1
 
-function reciveForm1() {
-    promisse = axios.post(quizzSend)  //post dando error, mas funciona com get (?)
+let quantQuestionQuizzNumber
+let quantNivelQuizzNumber
 
-    promisse.then(confirmation)
-    promisse.catch(erropost)
-}
+function saveValues(){
 
+    let tittleQuizzForm1 = document.querySelector(".tittlequizzform1").value
+    let urlQuizzForm1 = document.querySelector(".urlquizzform1").value
+    let quantQuestionQuizzForm1 = document.querySelector(".quantquestionquizzform1").value
+    let quantNivelQuizzForm1 = document.querySelector(".quantnivelquizzform1").value
 
-function confirmation (){
-    console.log("Thats Ok")
+    quantQuestionQuizzNumber = parseInt(quantQuestionQuizzForm1)
+    quantNivelQuizzNumber = parseInt(quantNivelQuizzForm1)
 
-    let tittlequizzform1 = document.querySelector(".tittlequizzform1").value
-    let urlquizzform1 = document.querySelector(".urlquizzform1").value
-    let quantquestionquizzform1 = document.querySelector(".quantquestionquizzform1").value
-    let quantnivelquizzform1 = document.querySelector(".quantnivelquizzform1").value
-
-    const sendArray = {
-        tittle: tittlequizzform1,
-        image: urlquizzform1,
-        questions: quantquestionquizzform1,
-        levels: quantnivelquizzform1,
+    if(tittleQuizzForm1 !== "" && urlQuizzForm1 !== "" &&  quantQuestionQuizzNumber === Number && quantNivelQuizzNumber === Number){
+        console.log("Ok!")
     }
 
+    else if(tittleQuizzForm1 === "" || urlQuizzForm1 === "" || quantQuestionQuizzForm1 === "" || quantNivelQuizzForm1 === ""){
+        alert("Preencha todos campos do formulário!")
+    }
 
-
+    else if(quantQuestionQuizzNumber !== Number || quantNivelQuizzNumber !== Number){
+        console.log("Quantidade de questões tem que ser um numero!")
+        console.log(quantQuestionQuizzNumber)
+        console.log(quantNivelQuizzNumber)
+    }
 }
 
-function erropost (){
-    console.log("Error")
-}
+// function reciveForm1() {
+//     promisse = axios.post(quizzSend)  //post dando error, mas funciona com get (?)
+
+//     promisse.then(confirmation)
+//     promisse.catch(erropost)
+// }
+
+
+// function confirmation (){
+//     console.log("Thats Ok")
+
+//     let tittlequizzform1 = document.querySelector(".tittlequizzform1").value
+//     let urlquizzform1 = document.querySelector(".urlquizzform1").value
+//     let quantquestionquizzform1 = document.querySelector(".quantquestionquizzform1").value
+//     let quantnivelquizzform1 = document.querySelector(".quantnivelquizzform1").value
+
+//     const sendArray = {
+//         tittle: tittlequizzform1,
+//         image: urlquizzform1,
+        
+//     }
+// }
+
+// function erropost (){
+//     console.log("Error")
+// }
